f37b98bfaf672ee5e6ac5d51589451eb3af51707
vansh-jain2002 <122167639+vansh-jain2002@users.noreply.github.com>
2024-07-28 23:46:57 +0530

    changes

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/Foodorderdb" ,{
    // useUnifiedTopology:true,
    // useNewUrlParser:true
+   useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => {
     console.log("connection succeful")
 }).catch((e) => {
